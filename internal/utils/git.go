3d83d160fd403204a57e0c2b8bbd7f5a71e26b78
self-sasi <sarthaks281bond@gmail.com>
2026-01-19 21:42:49 -0700

    change runGit scope to private and add GitDir helper for retrieving git dir path of the repo

--- internal/utils/git.go
+++ internal/utils/git.go
@@ -3,12 +3,13 @@ package utils
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 // RunGit is a utility method for running `git` with the provided flags
 // or subcommands. It returns a ("", error) tuple if the command executuon
 // fails, (output, nil) if successful.
-func RunGit(args ...string) (string, error) {
+func runGit(args ...string) (string, error) {
 	gitCmd := exec.Command("git", args...)
 
 	output, err := gitCmd.Output()
@@ -16,13 +17,13 @@ func RunGit(args ...string) (string, error) {
 		return "", err
 	}
 
-	return string(output), nil
+	return strings.TrimSpace(string(output)), nil
 }
 
 // VerifyInsideGitWorkTree runs `git rev-parse --is-inside-work-tree`
 // to check that we are in a git initialized repo. Returns error if not.
 func VerifyInsideGitWorkTree() error {
-	output, err := RunGit("rev-parse", "--is-inside-work-tree")
+	output, err := runGit("rev-parse", "--is-inside-work-tree")
 	if err != nil {
 		return err
 	}
@@ -31,4 +32,9 @@ func VerifyInsideGitWorkTree() error {
 		return fmt.Errorf(`Not a Git Repo`)
 	}
 	return nil
+}
+
+// GitDir returns the repo's git dir path (e.g. ".git" or a worktree git dir).
+func GitDir() (string, error) {
+	return runGit("rev-parse", "--git-dir")
 }
